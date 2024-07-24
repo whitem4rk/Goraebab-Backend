9cfd0d8cc05a1ea176453c7c5746ab859ec46b39
whitemark <whtmdgus56@nate.com>
2024-07-24 22:11:37 +0900

    [feature/#1] Init spring

---
+++ src/main/java/api/goraebab/GoraebabApplication.java
@@ -0,0 +1,13 @@
+package api.goraebab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoraebabApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GoraebabApplication.class, args);
+  }
+
+}

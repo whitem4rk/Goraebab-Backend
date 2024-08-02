5e31ba2bf4b9a953c4c9e505a829ba744fa751e1
whitemark <whtmdgus56@nate.com>
2024-08-02 23:21:26 +0900

    [feature/#5] Create storage repository

---
+++ src/main/java/api/goraebab/domain/remote/datebase/repository/StorageRepository.java
@@ -0,0 +1,8 @@
+package api.goraebab.domain.remote.datebase.repository;
+
+import api.goraebab.domain.remote.datebase.entity.Storage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StorageRepository extends JpaRepository<Storage, Long> {
+
+}

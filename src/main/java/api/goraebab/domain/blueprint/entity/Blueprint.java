74c2f3cbcc4f8981637e5b4a5f644f7ca97c4434
hershyYJ <juj0478@naver.com>
2024-08-06 01:55:49 +0900

    [feature/#10] Update blueprint entity

--- src/main/java/api/goraebab/domain/blueprint/entity/Blueprint.java
+++ src/main/java/api/goraebab/domain/blueprint/entity/Blueprint.java
@@ -1,12 +1,8 @@
 package api.goraebab.domain.blueprint.entity;
 
+import api.goraebab.domain.remote.database.entity.Storage;
 import api.goraebab.global.entity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,11 +19,32 @@ public class Blueprint extends BaseEntity {
   private Long id;
 
   @Column(nullable = false)
+  private String name;
+
+  @Lob
+  @Column(nullable = false)
   private String data;
 
+  @Column(nullable = false)
+  private Boolean isRemote;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "storage_id")
+  private Storage storage;
+
+  public void setAsRemote() {
+    this.isRemote = true;
+  }
+
+  public void setAsLocal() {
+    this.isRemote = false;
+  }
+
   @Builder
-  public Blueprint(String data) {
+  public Blueprint(String name, String data, Storage storage) {
+    this.name = name;
     this.data = data;
+    this.storage = storage;
   }
 
 }

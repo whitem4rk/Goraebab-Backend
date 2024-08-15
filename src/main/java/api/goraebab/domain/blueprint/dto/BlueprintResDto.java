2659cc3b5b3d4cb8464adc391fb2d0ba82f380f1
hershyYJ <juj0478@naver.com>
2024-08-15 17:25:07 +0900

    [feature/#18] Add constructor to BlueprintResDto

--- src/main/java/api/goraebab/domain/blueprint/dto/BlueprintResDto.java
+++ src/main/java/api/goraebab/domain/blueprint/dto/BlueprintResDto.java
@@ -1,9 +1,12 @@
 package api.goraebab.domain.blueprint.dto;
 
 import api.goraebab.domain.remote.database.dto.StorageResDto;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -11,5 +14,10 @@ import lombok.Setter;
 public class BlueprintResDto extends BlueprintsResDto {
 
     private StorageResDto storageInfo;
+
+    public BlueprintResDto(Long blueprintId, String name, String data, Boolean isRemote, LocalDateTime dateCreated, LocalDateTime dateUpdated, StorageResDto storageInfo) {
+        super(blueprintId, name, data, isRemote, dateCreated, dateUpdated);
+        this.storageInfo = storageInfo;
+    }
 
 }

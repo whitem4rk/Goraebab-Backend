cb614527a7b5505cf799d8636cc3ebaf83a78c28
whitemark <whtmdgus56@nate.com>
2024-09-28 23:50:33 +0900

    [docs/#31] Add all fields explanation of Blueprint Dtos

--- src/main/java/api/goraebab/domain/blueprint/dto/BlueprintReqDto.java
+++ src/main/java/api/goraebab/domain/blueprint/dto/BlueprintReqDto.java
@@ -1,5 +1,6 @@
 package api.goraebab.domain.blueprint.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -13,9 +14,11 @@ public class BlueprintReqDto {
 
     @NotBlank(message = "Name must not be blank")
     @Size(max = 255, message = "Name must be less than 255 characters")
+    @Schema(description = "The name of the blueprint.", example = "Project1 blueprint")
     private String name;
 
     @NotBlank(message = "Data must not be blank")
+    @Schema(description = "The raw data associated with the blueprint.")
     private String data;
 
 }

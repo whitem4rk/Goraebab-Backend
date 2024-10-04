e1af35b29b9d01fab76e354131d1b236d826c5cd
whitemark <whtmdgus56@nate.com>
2024-10-04 22:16:55 +0900

    [feature/#17] Remove Harbor

--- src/main/java/api/goraebab/domain/remote/harbor/repository/HarborRepository.java
+++
@@ -1,9 +0,0 @@
-package api.goraebab.domain.remote.harbor.repository;
-
-import api.goraebab.domain.remote.harbor.entity.Harbor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HarborRepository extends JpaRepository<Harbor, Long> {
-}

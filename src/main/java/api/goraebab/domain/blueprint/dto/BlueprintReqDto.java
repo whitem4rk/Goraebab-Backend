015273f220986747c982ab707bde2293a3b8e34a
hershyYJ <juj0478@naver.com>
2024-10-05 19:21:35 +0900

    [feature/#25] Updae data field type and new fields

--- src/main/java/api/goraebab/domain/blueprint/dto/BlueprintReqDto.java
+++ src/main/java/api/goraebab/domain/blueprint/dto/BlueprintReqDto.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @AllArgsConstructor
@@ -17,8 +18,18 @@ public class BlueprintReqDto {
     @Schema(description = "The name of the blueprint.", example = "Project1 blueprint")
     private String name;
 
-    @NotBlank(message = "Data must not be blank")
     @Schema(description = "The raw data associated with the blueprint.")
-    private String data;
+    private MultipartFile data;
+
+    @NotBlank(message = "IsDockerRemote must not be blank")
+    @Schema(description = "Indicates whether the blueprint is associated with a remote Docker repository.", example = "true or false")
+    private Boolean isDockerRemote;
+
+    @Schema(description = "The URL of the remote Docker repository if isDockerRemote is true.")
+    private String remoteUrl;
+
+    public void setData(MultipartFile data) {
+        this.data = data;
+    }
 
 }

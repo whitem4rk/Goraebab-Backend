d6a49d8fc97205f68587f3314b3e4e264cd860a7
hershyYJ <juj0478@naver.com>
2024-10-05 19:17:11 +0900

    [feature/#25] Add error handling for multipart file upload

--- src/main/java/api/goraebab/global/exception/ErrorCode.java
+++ src/main/java/api/goraebab/global/exception/ErrorCode.java
@@ -40,7 +40,8 @@ public enum ErrorCode {
      */
 
     SAVE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, 500, "Failed to save blueprint."),
-    MODIFY_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, 500, "Failed to modify blueprint.");
+    MODIFY_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, 500, "Failed to modify blueprint."),
+    FILE_PROCESSING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500, "Failed to process file content.");
 
 
     private final HttpStatus status;

1af3d341a8dd72dc71186353b1b6b8fb50f306dd
whitemark <whtmdgus56@nate.com>
2024-10-22 16:44:00 +0900

    [feature/#45] Modify BlueprintReqDto  format

--- src/main/java/api/goraebab/domain/blueprint/dto/BlueprintReqDto.java
+++ src/main/java/api/goraebab/domain/blueprint/dto/BlueprintReqDto.java
@@ -1,35 +1,37 @@
 package api.goraebab.domain.blueprint.dto;
 
+import api.goraebab.domain.blueprint.dockerObject.ProcessedData;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class BlueprintReqDto {
 
     @NotBlank(message = "Name must not be blank")
     @Size(max = 255, message = "Name must be less than 255 characters")
     @Schema(description = "The name of the blueprint.", example = "Project1 blueprint")
-    private String name;
+    private String blueprintName;
 
+    @NotBlank(message = "Data must not be blank")
     @Schema(description = "The raw data associated with the blueprint.")
-    private MultipartFile data;
+    private String data;
 
-    @NotBlank(message = "IsDockerRemote must not be blank")
-    @Schema(description = "Indicates whether the blueprint is associated with a remote Docker repository.", example = "true or false")
-    private Boolean isDockerRemote;
+    @NotNull(message = "ProcessedData must not be blank")
+    @Schema(description = "Parsed data associated with the blueprint.")
+    private ProcessedData processedData;
 
-    @Schema(description = "The URL of the remote Docker repository if isDockerRemote is true.")
-    private String remoteUrl;
 
-    public void setData(MultipartFile data) {
+    @Builder
+    public BlueprintReqDto(String blueprintName, String data, ProcessedData processedData) {
+        this.blueprintName = blueprintName;
         this.data = data;
+        this.processedData = processedData;
     }
 
 }

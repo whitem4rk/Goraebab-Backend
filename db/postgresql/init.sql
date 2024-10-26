1e0d06ca9658aa250a6644447f223469bfe565c1
whitemark <whtmdgus56@nate.com>
2024-10-26 21:56:07 +0900

    [feature/#37] Add postgresql config

--- db/postgresql/init.sql
+++ db/postgresql/init.sql
@@ -1,4 +1,5 @@
-CREATE DATABASE goraebab
+CREATE
+DATABASE goraebab
     WITH
     OWNER = postgres
     ENCODING = 'UTF8'
@@ -8,3 +9,38 @@ CREATE DATABASE goraebab
     CONNECTION LIMIT = -1;
 
 \c goraebab;
+
+CREATE TABLE daemon
+(
+    id           BIGSERIAL PRIMARY KEY,
+    host         VARCHAR(255) NOT NULL,
+    port         INTEGER      NOT NULL,
+    name         VARCHAR(255) NOT NULL,
+    date_created TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    date_updated TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE storage
+(
+    id       BIGSERIAL PRIMARY KEY,
+    host     VARCHAR(255) NOT NULL,
+    port     INTEGER      NOT NULL,
+    dbms     VARCHAR(50)  NOT NULL,
+    name     VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    date_created TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    date_updated TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE blueprint
+(
+    id         BIGSERIAL PRIMARY KEY,
+    name       VARCHAR(255) NOT NULL,
+    data       TEXT        NOT NULL,
+    is_remote  BOOLEAN,
+    storage_id BIGINT,
+    FOREIGN KEY (storage_id) REFERENCES storage (id),
+    date_created TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    date_updated TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP
+);

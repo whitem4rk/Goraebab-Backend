4141b754d492d9652e7e9cae752df407ed69db96
whitemark <whtmdgus56@nate.com>
2024-12-01 14:42:29 +0900

    [docs/#71] Add Javadoc for daemon connection service

--- src/main/java/api/goraebab/domain/remote/docker/service/DaemonService.java
+++ src/main/java/api/goraebab/domain/remote/docker/service/DaemonService.java
@@ -4,6 +4,14 @@ import api.goraebab.domain.remote.docker.dto.DaemonReqDto;
 import api.goraebab.domain.remote.docker.dto.DaemonResDto;
 import java.util.List;
 
+/**
+ * Service interface for managing daemon entities and their operations.
+ *
+ * <p>This interface provides methods for retrieving, connecting and deleting daemon configurations.</p>
+ *
+ * @author whitem4rk
+ * @version 1.0
+ */
 public interface DaemonService {
 
   List<DaemonResDto> getDaemons();
